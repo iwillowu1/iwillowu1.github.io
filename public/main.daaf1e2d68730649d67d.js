3b7737ce9b039d65f97fcf1a4cc60a224ebdb44c
jaredcompton <jcompton@willowinc.com>
2020-06-10 14:33:57 +1000

    test

---
+++ public/main.daaf1e2d68730649d67d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{tjUo:function(e,n,t){"use strict";t.r(n);var o=t("g0WC"),r=t("ERkP"),c=t.n(r),a=t("bk/2"),s=t.n(a);Object(o.setConfig)({reloadHooks:!1});const l=function(){return c.a.createElement("div",null,"test")};s.a.render(c.a.createElement(l,null),document.getElementById("root"))}},[["tjUo",1,2]]]);
